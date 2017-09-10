467115d940fb6e5107b3ce4e64591c9cbfc6d94b
Harold Kim <root@stypr.com>
2017-09-10 14:48:05 +0900

    Logging and other bug fixes

--- model.php
+++ model.php
@@ -2,7 +2,7 @@
 
 	require("query.php");
 
-	// Not Implemented!
+	// not implemented as yet. (should probably be there in future)
 	class Badges {
 		public $badge_id;
 		public $badge_list;
@@ -10,7 +10,7 @@
 
 	/* Player Information */
 	class Player {
-		public $user_no;
+		public $user_no = "";
 		public $user_id;
 		public $user_pw;
 		public $user_nickname;
@@ -65,8 +65,8 @@
 			foreach($res as $key => $val){
 				$player->$key = $val;
 			}
-			foreach($user_controllable as $control){
-				$player->$control = (string)$res[$val];
+			foreach($this->user_controllable as $control){
+				$player->$control = (string) $player->$control;
 			}
 			// customized input
 			$player->user_rank = (int)((@$res['rank']) ?
@@ -97,18 +97,27 @@
 		public function set(Player $player){
 			$user_check = $this->get_by_username($player->user_id);
 			$player = $this->input_filter($player);
-			if($user_check->user_id === $player->user_id){
-				// update by diff
-				$diff_curr = get_object_vars($player);
-				$diff_prev = get_object_vars($user_check);
-				$diff = array_diff($diff_curr, $diff_prev);
-				$query = "UPDATE user SET ";
-				foreach($diff as $key => $val){
-					$query .= $key . "='" . $val . "', ";
+			if($user_check->user_id === $player->user_id &&
+			$player->user_id != NULL){
+				if(!$player->user_no){
+                    // delete if index is null
+                    $name = $this->db->filter($player->user_id);
+                    $query = "DELETE FROM user WHERE user_id='$name'";
+                    $r = $this->db->query($query);
+				}else{
+					// update by diff
+					$diff_curr = get_object_vars($player);
+					$diff_prev = get_object_vars($user_check);
+					$diff = array_diff($diff_curr, $diff_prev);
+					$query = "UPDATE user SET ";
+					foreach($diff as $key => $val){
+						$query .= $key . "='" . $val . "', ";
+					}
+					// remove last two trailling characters
+					$query = substr($query, 0, -2);
+					$query .= "WHERE username='$player->user_id'";
+					$r = $this->db->query($query);
 				}
-				$query = substr($query, 0, -2); // remove last two trailing characters
-				$query .= "WHERE username='$player->user_id'";
-				$r = $this->db->query($query);
 			}else{
 				// insert by query
 				$key=""; $val ="";
@@ -130,7 +139,7 @@
 
 	/* Challenge Information */
 	class Challenge {
-		public $challenge_id;
+		public $challenge_no = "";
 		public $challenge_name;
 		public $challenge_desc;
 		public $challenge_score;
@@ -163,7 +172,7 @@
 		public function get_by_name(string $name): Challenge {
 		$name = $this->db->filter($name);
 			$res = $this->db->query("SELECT * FROM chal WHERE challenge_name='$name'", 1);
-			return ($res) ? $res : new Challenge;
+			return ($res) ? $this->parse_challenge($res) : new Challenge;
 		}
 		public function get_by_flag(string $flag): Challenge {
 			// get by flag
@@ -180,7 +189,7 @@
 			}
 			$res = $this->db->query("SELECT * FROM chal WHERE challenge_flag='$flag'".
 									"AND challenge_is_open=1", 1);
-			return ($res) ? parse_challenge($res) : new Challenge;
+			return ($res) ? $this->parse_challenge($res) : new Challenge;
 		}
 		public function get_list(bool $all=false): array {
 			// Loads list of challenges.
@@ -192,22 +201,35 @@
 			return ($res) ? $res : Array();
 		}
 		public function get_solver(Challenge $chall): array{
+			// is a TODO again; this is inefficient
+			$log = new LoggingInfo($this->db);
+			var_dump($log->get_by_challenge($chall->challenge_name));
+			return Array();
 		}
 		public function set(Challenge $chall): bool{
 			// insert if new, update if non-exist
-			$chall_check = $this->get_by_name($chall->challenge_id);
-			if($chall_check->$challenge_id === $chall->challenge_id){
+			$chall_check = $this->get_by_name($chall->challenge_name);
+			if($chall_check->$challenge_id === $chall->challenge_id &&
+			$chall_challenge_id != NULL){
 				// update by diff
-				$diff_curr = get_object_var($chall);
-				$diff_prev = get_object_vars($chall_check);
-                $diff = array_diff($diff_curr, $diff_prev);
-				$query = "UPDATE chal SET ";
-				foreach($diff as $key => $val){
-					$query .= $key . "='" . (string)$val. "', ";
+				if(!$diff_curr->challenge_no){
+					// delete if index is null
+					$name = $this->db->filter($chall->challenge_name);
+					$query = "DELETE FROM chal WHERE challenge_name='$name'";
+					$r = $this->db->query($query);
+				}else{
+					// update if nothing's wrong..
+					$diff_curr = get_object_var($chall);
+					$diff_prev = get_object_vars($chall_check);
+					$diff = array_diff($diff_curr, $diff_prev);
+					$query = "UPDATE chal SET ";
+					foreach($diff as $key => $val){
+						$query .= $key . "='" . (string)$val. "', ";
+					}
+					$query = substr($query, 0, -2);
+					$query .= "WHERE challenge_id='$chall->chall_id'";
+					$r = $this->db->query($query);
 				}
-				$query = substr($query, 0, -2);
-				$query .= "WHERE challenge_id='$chall->chall_id'";
-				$r = $this->db->query($query);
 			}else{
 				// insert by query
 				$key=""; $val="";
@@ -221,14 +243,14 @@
 				$val = substr($val, 0, -1);
 				$query = "INSERT INTO user ($key) VALUES ($val)";
 				$r = $this->db->query($query);
-            }
+			}
 			return $r;
 		}
 	}
 
 	/* Logging Information */
 	class Logging {
-		public $log_no;
+		public $log_no = "";
 		public $log_id;
 		public $log_type;
 		public $log_challenge;
@@ -237,25 +259,78 @@
 	}
 
 	interface LoggingInterface {
-		public function get_by_id(string $name): array;
+		public function get_by_username(string $name): array;
 		public function get_by_type(string $type): array;
 		public function get_by_challenge(string $chall): array;
+		public function get_by_no(string $no): Logging;
 		public function set(Logging $log): bool;
 	}
 
 	class LoggingInfo implements LoggingInterface {
 		protected $db;
-		public function __construct(){ $this->$db = $db }
-		public function get_by_id(string $name): array {
-			// ..?
+		public function __construct($db){ $this->db = $db; }
+		private function parse_log(array $res): Logging {
+			$log = new Logging;
+			foreach($res as $key => $val){
+				$log->$key = $val;
+			}
+			return $log;
 		}
-		public function get_by_type(string $type ): array {
-
+		public function get_by_username(string $name): array {
+			$name = $this->db->filter($name);
+			$res = $this->db->query("SELECT * FROM log WHERE log_id='$name'", 2);
+			for($i=0;$i<count($res);$i++){ $res[$i] = $this->parse_log($res[$i]); }
+			return ($res) ? $res : Array();
+		}
+		public function get_by_type(string $type): array {
+			$type = $this->db->filter($type);
+			$res = $this->db->query("SELECT * FROM log WHERE log_type='$type'", 2);
+			var_dump($res);
+			for($i=0;$i<count($res);$i++){ $res[$i] = $this->parse_log($res[$i]); }
+			return ($res) ? $res : Array();
 		}
 		public function get_by_challenge(string $chall): array {
+			$chall = $this->db->filter($chall);
+            $res = $this->db->query("SELECT * FROM log WHERE log_challenge='$chall'", 2);
+            for($i=0;$i<count($res);$i++){ $res[$i] = $this->parse_log($res[$i]); }
+            return ($res) ? $res : Array();
+		}
+		public function get_by_no(string $no): Logging {
+			$no = $this->db->filter($no);
+			$res = $this->db->query("SELECT * FROM log WHERE log_no='$no'", 1);
+			return ($res) ? $this->parse_log($res) : new Logging;
 		}
 		public function set(Logging $log): bool {
-
+			// insert if new, update if non-exist
+			$log_check = $this->get_by_no($log->log_no);
+			if($log_check->log_no === $log->log_no && $log->log_no != NULL){
+				// update by diff
+				$diff_curr = get_object_vars($log);
+				$diff_prev = get_object_vars($log_check);
+				$diff = array_diff($diff_curr, $diff_prev);
+				$query = "UPDATE log SET ";
+				foreach($diff as $key => $val){
+					$query .= $key . "='" . (string)$val. "', ";
+				}
+				$query = substr($query, 0, -2);
+				$query .= "WHERE log_no='$log->log_no'";
+				$r = $this->db->query($query);
+			}else{
+				// insert by query
+				$key=""; $val="";
+				$p = get_object_vars($log);
+				$p['log_no'] = null;
+				$p['log_date'] = ($p['log_date']) ? $p['log_date'] : date("Y-m-d H:i:s");
+				foreach($p as $k => $v){
+					$key .= "$k,";
+					$val .= ($val)? "'$v'," : "NULL,";
+				}
+				$key = substr($key, 0, -1);
+				$val = substr($val, 0, -1);
+				$query = "INSERT INTO log ($key) VALUES ($val)";
+				$r = $this->db->query($query);
+            }
+			return $r;
 		}
 	}
 
