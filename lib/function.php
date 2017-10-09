da60123a1f966f6041d344764d4709046a8532ad
Harold Kim <root@stypr.com>
2017-10-09 14:34:25 +0900

    Added more comments and divided scripts specifically

---
+++ lib/function.php
@@ -0,0 +1,42 @@
+<?php
+
+/* lib/function.php
+Global function set */
+
+function redirect($page) {
+	// Redirect function: use at your own risk!
+	header( "Location: /" . $page );
+	exit;
+}
+
+function secure_hash(string $str): string {
+	// (Hopefully) Secure hash
+	return sha1( sha1( md5( $str ) ) . __HASH_SALT__ );
+}
+
+function generate_random_string(int $len = 40): string {
+	// Generate CSPRNG random string
+	$code = '';
+	$_table = '0123456789ABCDEFGHIJKLMNOPQRSTUVWYZabcdefghijklmnopqrstuvwxyz';
+	$_table_len = strlen( $_table ) - 1;
+	for ( $i=0; $i<$len; $i++ ) {
+		$code .= $_table[random_int( 0,$_table_len )];
+	}
+	return $code;
+}
+
+function update_wechall(string $nickname = ""){
+	// Remote update WeChall Profile
+	global $query;
+	$site = urlencode( "Stereotyped Challenges" );
+	$nick = ($nickname) ? $query->filter($nickname, "sql") : $_SESSION['nickname'];
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, "http://www.wechall.net/remoteupdate.php?sitename=$site&username=$nick");
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$result = curl_exec($ch);
+	curl_close($ch);
+	return $result;
+}
+
+?>
--- lib/init.php
+++ lib/init.php
@@ -1,68 +1,28 @@
 <?php
 
-	/* Basic Initialization
-	This file MUST be initialized from the very beginning of every pages. */
+/* lib/init.php
+Init/Load configurations, estabilish sql connection */
 
-	// hide errors
-	ini_set("display_errors", "off");
-	error_reporting(0);
+// Hide Errors: debug.php for error output //
+ini_set("display_errors", "off");
+error_reporting(0);
 
-	// load config, once.
-	require_once("exclude/config.php");
+// Session Timeout (thx to madbat2) //
+session_set_cookie_params(3600 * 48);
+session_cache_expire(3600 * 48);
+ini_set("session.gc_maxlifetime", 3600 * 48);
+session_name("chall");
+session_start();
 
-	// thx to madbat2
-	session_set_cookie_params(3600 * 48);
-	session_cache_expire(3600 * 48);
-	ini_set("session.gc_maxlifetime", 3600 * 48);
-	session_name("chall");
-	session_start();
-	assert(strlen(__HASH_SALT__) >= 60) || die("Your service is insecure!");
+// Load Configuration and Test //
+include_once( "exclude/config.php" );
+if ( __INSTALL__ !== true ) die( "Not installed" );
+assert( strlen( __HASH_SALT__ ) >= 60 ) || die( "Insecure Salt" );
 
-	// init query
-	$query = new Query();
-	$query->connect(__DB_HOST__, __DB_USER__, __DB_PASS__, __DB_BASE__);
-	if($query->check() == false) die("SQL server's down :(");
-
-	// basic stuff..
-	function redirect($page){
-		header("Location: /".$page);
-		exit;
-	}
-	function secure_hash(string $str): string {
-		return sha1(sha1(md5($str)) . __HASH_SALT__);
-	}
-
-	// update via wechall
-	function update_wechall($nickname=""){
-		global $query;
-		$nickname = ($nickname) ? $query->filter($nickname, "sql") : $_SESSION['nickname'];
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, "http://www.wechall.net/remoteupdate.php?sitename=Stereotyped%20Challenges&username=".$query->filter($nickname, "sql"));
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		$result = curl_exec($ch);
-		curl_close($ch);
-		return $result;
-	}
-
-	// simple method to return error.
-	function return_error() {
-		http_response_code(404);
-		$template = new Template();
-		$template->include("error");
-	}
-
-	// fatal catcher
-	function shutdown_function(){
-		global $query;
-		$error = error_get_last();
-		unset($query);
-		// fatal error, E_ERROR === 1
-		if ($error['type'] === E_ERROR){
-			//var_dump($error);
-			return_error();
-		}
-    }
-    register_shutdown_function('shutdown_function');
+// Initialize Query //
+require_once( "query.php" );
+$query = new Query();
+$query->connect(__DB_HOST__, __DB_USER__, __DB_PASS__, __DB_BASE__);
+if($query->check() == false) die("MySQL Down");
 
 ?>
